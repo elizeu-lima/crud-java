f9a86e88377ba5948079cceefe9ac96eb9db97f5
elizeu-lima <elizeucoca@gmail.com>
2026-02-23 10:13:02 -0300

    ajustando o corsconfig

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins("*") // Permite QUALQUER origem (Vercel, Localhost, etc.)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false); // ATENÇÃO: Ao usar "*", allowCredentials deve ser false
     }
 }
